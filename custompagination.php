8bfc4b093679e4867cce95795fbf6588c47602fc
Dimitry Zolotaryov <dimitry@webit.ca>
2014-12-27 14:07:58 -0500

    Version 1.0

---
+++ custompagination.php
@@ -0,0 +1,136 @@
+<?php
+/*
+Plugin Name: Custom Pagination
+Description: Turns post links with the Link URL 'page:next|prev|first|last|1|2|3|...' into an actual link to the corresponding page. If the page is invalid, the link is hidden. Developed for DateDaily.com 
+Author: Dimitry Zolotaryov
+Version: 1.0
+Author URI: http://webit.ca
+*/
+
+add_action( 'the_content', 'cp_parse_content' );
+add_action( 'admin_head', 'cp_in_admin' );
+
+// indicates the number of links found
+$_cp_links_found = 0;
+
+// true indicates that the request is for the admin section
+$_cp_in_admin = false;
+
+/**
+ * Flags the request as being in the admin.
+ * 
+ */
+function cp_in_admin() {
+	global $_cp_in_admin;
+	$_cp_in_admin = true;
+}
+
+/**
+ * Parses the text in search of marks to link to a different page.
+ *
+ * @param string $text The body of the post
+ * @return string The body, formatted
+ */
+function cp_parse_content( $text ) {
+	global $_cp_links_found, $_cp_in_admin;
+	if ( $_cp_in_admin ) return $text;
+	$_cp_links_found = 0;
+	return preg_replace_callback(
+		'/<a[^>]+href="page:(\d|next|previous|prev|first|last)"[^>]*>(.*)<\\/a>/siU',
+		'_cp_replace_callback', $text );
+	
+}
+
+/**
+ * Function returns true if there were links found in the text of
+ * the post. Otherwise, a false is returned.
+ * 
+ * @return boolean True or false
+ */
+function cp_link_found() {
+	global $_cp_links_found;
+	return $_cp_links_found > 0;
+}
+
+/**
+ * Callback function called by a custom regular expression that finds links
+ * to pages of a post.
+ * 
+ * For: /<a[^>]+href="page:(\d|next|previous|prev)"[^>]*>([^<]*)<\\/a>/
+ *
+ * @param array $matches Requires 3 matches: The full link, the page ID, and 
+ * 						  the content in the link.
+ * @return str The formated string or only the text
+ */
+function _cp_replace_callback( $matches ) {
+	global $post, $page, $numpages, $_cp_links_found;
+	
+	$_cp_links_found++;
+	
+	if ( count( $matches ) < 3 ) {
+		return $matches[ 0 ];
+	}
+	$pageid = $matches[ 1 ];
+	$linkto = false;
+	switch ( strtolower( $matches[ 1 ] ) ) {
+		case 'first':
+			if ( $page != 1 ) {
+				$linkto = 1;
+			}
+			break;
+		case 'last':
+			if ( $page != $numpages ) {
+				$linkto = $numpages;
+			}
+			break;
+		case 'next':
+			if ( $page < $numpages ) {
+				$linkto = $page + 1;
+			}
+			break;
+		case 'prev':
+		case 'previous':
+			if ( $page > 1 ) {
+				$linkto = $page - 1;
+			}
+			break;
+		default:
+			$pageid = intval( $pageid );
+			if ( $pageid > 0 || $pageid <= $numpages ) {
+				$linkto = $pageid;
+			}
+	}
+	if ( $linkto !== false ) {
+		
+		$anchor = $matches[ 0 ];
+		
+		// builds the URL structure
+		if ( '' == get_option('permalink_structure')
+			 || in_array($post->post_status, array('draft', 'pending')) )
+			$link = get_permalink() . '&amp;page=' . $linkto;
+		else
+			$link = trailingslashit(get_permalink())
+					. user_trailingslashit($linkto, 'single_paged');
+
+		// adds the class name "page" to the link
+		$classname = ' page ';
+		if ( strpos( $matches[ 0 ], 'class="' ) !== false ) {
+			$anchor = str_replace( 'class="',
+								   'class="' . $classname,
+									$anchor );			
+		} else {
+			$anchor = str_replace( '<a ',
+								   '<a class="' . $classname . '" ',
+								   $anchor );
+		}
+		
+		return str_replace( 'href="page:' . $pageid . '"',
+							'href="' . $link . '"',
+							$anchor );
+	} else {
+		// no link, no text
+		return '';
+	}
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,61 @@
+=== Plugin Name ===
+Contributors: dimitryz
+Funded by: Mate1 Inc.
+Tags: editing, pagination
+Requires at least: 2.8
+Tested up to: 2.8
+Stable tag: 1.0
+
+The Custom Pagination plugin allows a user to insert custom next, previous or numbered page links into a post.
+
+== Description ==
+
+With the Custom Pagination plugin, a user may customize the look of a post's next and previous links using the HTML editor. It is built by [WebIT.ca (Dimitry Zolotaryov)](http://webit.ca) and funded by [DateDaily.com](http://datedaily.com): an online dating and relationship blog.
+
+The link is created by selecting the text label within a post's body and clicking the link icon. In the Link URL field, the following values will produce a link to another page:
+
+* _page:next_ links to the next page
+* _page:prev_ or _page:previous_ links to the previous page
+* _page:first_ links to the first page of the post
+* _page:last_ links to the last page of a post
+* _page:n_ links to the nth page of a post (e.g. page:2 for the second page)
+
+If link is directing to a page that does not exist -- for instance _page:next_ on the last page --, the link and the link text do not appear.
+
+All links processed by Custom Pagination will have the added class name 'page'. If you wish to style such links, in your CSS, add the line:
+
+    a.page { /* page style goes here */ }
+
+For more information, visit the [Custom Pagination plugin homepage](http://webit.ca/2009/08/custom-pagination-plugin-for-wordpress/).
+
+== Screenshots ==
+
+1. Selecting text for a link to the next page
+2. Adding the next page link text
+3. The generated link
+
+== Installation ==
+
+1. Copy or upload the `custom-pagination` directory into your `/wp-content/plugins/` directory 
+1. If you downloaded a zipped copy (i.e. `custom-pagination.zip`), use the 'Plugins' > 'Add New' section of WordPress to upload it
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+Done.
+
+Actually, if you wish to use WordPress pagination when there is no Custom Pagination on a page, you may use the following PHP code:
+
+	// inside single.php
+	// displays the standard pagination when no custom pagination link is found
+	if ( ! cp_link_found() )
+  		wp_link_pages(array('next_or_number' => 'next', 'nextpagelink'=>'Next page &raquo;', 'previouspagelink'=>'&laquo; Previous page'));	
+  	 
+
+== Frequently Asked Questions ==
+
+*Will the pagination work with custom permalinks?*
+
+It should.
+
+*What is the cost of the plugin?*
+
+This plugin is provided free of charge thanks to the good people at [Mate1 Inc](http://mate1.com) and the site [DateDaily.com](http://datedaily.com). You may use the Custom Pagination plugin for any purpose provided you keep the comment section of the custompagination.php file.
